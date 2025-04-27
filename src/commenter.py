2308828d443f14d815391f68126c430e1852becd
Richard Cruz-Silva <rsc2174@columbia.edu>
2025-04-26 21:35:20 -0700

    fixed dataset.json and got BLEU evaluator to work

---
+++ src/commenter.py
@@ -0,0 +1,19 @@
+import json
+import nltk
+
+def load_json_file(file_path):
+  try:
+    with open(file_path, 'r') as file:
+      return json.load(file)
+  except FileNotFoundError:
+    print(f"Error: File not found at {file_path}")
+  except json.JSONDecodeError as e:
+    print(f"Error: Failed to decode JSON - {e}")
+
+if __name__ == "__main__":
+  file_path = "data/dataset.json"
+  dataset = load_json_file(file_path)
+  if dataset:
+    for item in dataset:
+      BLEUscore = nltk.translate.bleu_score.sentence_bleu([item['code']], item['annotated_code'])
+      print(f"BLEU score for {item['id']}: {BLEUscore}")
